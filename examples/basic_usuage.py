929f01efdfe6983ab1ba17899ed4ef3aff2df406
Abdul Karim <karim.develops@gmail.com>
2026-09-05 23:42:09 +0500

    refactor: increase limit in example

--- examples/basic_usuage.py
+++ examples/basic_usuage.py
@@ -3,7 +3,7 @@ from fastapi import FastAPI
 from tortle import SlidingLogLimiter
 
 app = FastAPI()
-app.add_middleware(SlidingLogLimiter, limit=1, window=120)
+app.add_middleware(SlidingLogLimiter, limit=8, window=120)
 
 
 @app.get("/")
